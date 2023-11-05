dcef6badd8fd8cf2a538ff567f093dab64ccc431
Jacoai <banan0012354@gmail.com>
2023-11-05 13:51:50 +0300

    add exports

--- lib/virus_total_cli.dart
+++ lib/virus_total_cli.dart
@@ -1,4 +1,7 @@
 library;
 
 export 'src/api/virus_total_client.dart';
+export 'src/api/models/analysis_data_model/analysis_data_model.dart';
+
 export 'src/database/database_client.dart';
+export 'src/database/model/database_data_model.dart';
