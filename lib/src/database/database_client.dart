d0eb1625ff70a4368c955079e6c14fd674424f9d
Jacoai <banan0012354@gmail.com>
2023-11-12 18:32:17 +0300

    refactor regular expression for isFile

--- lib/src/database/database_client.dart
+++ lib/src/database/database_client.dart
@@ -93,7 +93,7 @@ class DatabaseClient {
 
   bool _isFile(String path) {
     RegExp urlRegex = RegExp(
-        r'[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)');
+        r"((https?:www\.)|(https?:\/\/)|(www\.))[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}(\/[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)?");
 
     if (path.contains(urlRegex)) {
       return false;
