82949eca62dc9000154e77c2934910fa0382abf7
Jacoai <banan0012354@gmail.com>
2023-11-12 15:16:56 +0300

    refactor: change name and fileld

---
+++ lib/src/database/converter/converter_analysis_data_to_virus_total_data.dart
@@ -0,0 +1,16 @@
+import 'package:virus_total_cli/src/api/models/analysis_data_model/analysis_data_model.dart';
+import 'package:virus_total_cli/src/database/model/database_data_model.dart';
+
+VirusTotalData convertAnalysisDataToVirusTotalData(
+    AnalysisData data, String source, bool isFile) {
+  return VirusTotalData(
+    harmless: data.attributes.stats.harmless,
+    malicious: data.attributes.stats.malicious,
+    suspicious: data.attributes.stats.suspicious,
+    timeout: data.attributes.stats.timeout,
+    undetected: data.attributes.stats.undetected,
+    time: data.attributes.date,
+    source: source,
+    isFile: isFile,
+  );
+}
